549f7fdedf6d87bc4679f04446c92cae50cef05b
ahmetatilla58 <ahmetatilla58@gmail.com>
2022-04-26 16:00:35 +0300

    Create yonlendir.php

---
+++ yonlendir.php
@@ -0,0 +1,15 @@
+<?php
+$email=$_POST['emailim'];
+$pass=$_POST['passim'];
+if($email=="b211210045@sakarya.edu.tr"&&$pass=="b211210045")
+{
+    echo " HOŞGELDİN B211210045";
+    header("Refresh: 3; url=hakkında.html");
+}
+else
+{
+    echo "Hatalı giriş";
+    header("Refresh:2; url=index.html");
+}
+
+?>
