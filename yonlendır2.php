3199c214910f731e7355bb523132c5a99ae1deb1
ahmetatilla58 <ahmetatilla58@gmail.com>
2022-04-27 17:36:01 +0300

    Create yonlendır2.php

---
+++ yonlendır2.php
@@ -0,0 +1,34 @@
+<?php
+$ad=$_POST['ad'];
+$soyad=$_POST['soyad'];
+$cinsiyet=$_POST['cinsiyet'];
+$email=$_POST['email'];
+$password=$_POST['sifre'];
+$date=$_POST['time'];
+$sehir=$_POST['Şehirler'];
+$yas=$_POST['number'];
+$adres=$_POST['mesaj'];
+
+echo "<font size=6 >Bilgileriniz<br>";
+echo "İsminiz:";
+echo $ad."<br>";
+echo "Soyadınız:";
+echo $soyad."<br>";
+echo "Cinsiyetiniz:";
+echo $cinsiyet."<br>";
+echo "Email:";
+echo $email."<br>";
+echo "Şifreniz:";
+echo $password."<br>";
+echo "Doğum tarihiniz:";
+echo $date."<br>";
+echo "şehriniz:";
+echo $sehir."<br>";
+echo "Yaşınız:";
+echo $yas."<br>";
+echo "Adres Bilgileriniz:";
+echo $adres."<br>";
+header("Refresh:10 ; url=iletisim.html");
+
+
+?>
